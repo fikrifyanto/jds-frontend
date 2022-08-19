3bd4ccff8ba9b672220ba1918055dee8be5b8441
Fikri <fikrifyanto@gmail.com>
2022-08-19 22:33:13 +0700

    feat(init): init project and add form page
    Init project for starting development
    
    Feature #01

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template class>
+  <div class="flex justify-center py-20 bg-slate-50 min-h-screen">
+    <Form />
+  </div>
+</template>
+
+<script>
+import Form from "./components/Form.vue";
+
+export default {
+  name: "JDS-Form",
+  components: { Form },
+};
+</script>
+
+<style></style>
